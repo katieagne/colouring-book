c4ce92ce13b8b680ae0b5f41189655334dc46c2b
katieagne <katieagne@gmail.com>
2021-11-18 18:36:05 -0800

    Create file structure

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Home from "./pages/Home/Home";
+import Dashboard from "./pages/Dashboard/Dashboard";
+
+// TODO create routes
+
+function App() {
+  return (
+    <div className="App">
+      in app.jsx
+      <Home />
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
